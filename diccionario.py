962b1bab32788c164fd1d3ba8ecfe61d7fd70af6
Salamanca1927 <143855030+Salamanca1927@users.noreply.github.com>
2023-09-01 20:34:39 -0500

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,27 @@
+# dic = {'c1':['a','b','c'],'c2':['d','e','f']}
+# print(dic ['c2'][1].upper())
+
+estudiantes = {
+    "id":123,
+    "name":"Andrea",
+    "subjet":["Bd","Web II"]
+}
+
+estudiantes1 = {
+    "id":124,
+    "name":"Alejandro",
+    "subjet":["Lógica"]
+}
+
+estudiantes.update(estudiantes1)
+# for clave, valor in estudiantes.item():
+#     estudiantes1[clave] = valor
+    
+print(estudiantes)
+
+# estudiantes["celphone"] = 3107066178
+# estudiantes["name"] = "Andrés"
+
+
+
+
